4c265e2b51b69e63c5c07270140f1c3f1c821163
Evgeniy Srvostyanov <exocornet@gmail.com>
2021-02-03 17:15:11 +0300

    реализация меню

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './scss/style.scss';
 
 document.querySelector('.menu-hamburger').onclick = function () {
   document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
-  document.querySelector('.menu').classList.toggle('menu_active');
-  document.querySelector('.wrapper-logo-menu').classList.toggle('wrapper-logo-menu_active');
+  document.querySelector('.menu-pages__click-hamburger').classList.toggle('menu-pages__click-hamburger_active');
+  document.querySelector('.contacts').classList.toggle('contacts_active');
 }
