c33111643ff7e3ff5567a0304deb19a817bb2638
Evgeniy Srvostyanov <exocornet@gmail.com>
2021-02-03 18:51:20 +0300

    mobile menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './scss/style.scss';
 
 document.querySelector('.menu-hamburger').onclick = function () {
   document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
-  document.querySelector('.menu-pages__click-hamburger').classList.toggle('menu-pages__click-hamburger_active');
+  document.querySelector('.block-click-hamburger').classList.toggle('block-click-hamburger_active');
   document.querySelector('.contacts').classList.toggle('contacts_active');
 }
