55e43655f53ba2afa05d095d9b0a0fdb3786721b
Evgeniy Srvostyanov <exocornet@gmail.com>
2021-02-04 11:45:55 +0300

    модальное окно обратной связи

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
   console.log(scrollbar);
   document.querySelector('[href="#openModal"]').addEventListener('click', function () {
-    // document.body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
     document.querySelector('#openModal').style.marginLeft = scrollbar;
   });
   document.querySelector('[href="#close"]').addEventListener('click', function () {
